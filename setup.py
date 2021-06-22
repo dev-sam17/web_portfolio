14c7400b5f54303689889751acce54f22040f6a6
dev-sam17 <70699895+dev-sam17@users.noreply.github.com>
2021-06-22 14:18:54 +0530

    update

--- setup.py
+++ setup.py
@@ -1,16 +1,28 @@
-from flask import Flask,render_template,request,url_for
+from flask import Flask, render_template, request, url_for, redirect
 from markupsafe import escape
 import csv
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
-@app.route('/<name>')
-def dynamic_url(name=None):
-    return render_template(f'{name}',name=name)
+# @app.route('/')
+# def index():
+#     return redirect(url_for('home'))
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+# @app.route('/works.html')
+# def works():
+#     return render_template('works.html')
+
+# @app.route('/contact.html')
+# def contact():
+#     return render_template('contact.html')
 
 
 # def writetofile(data):
@@ -21,23 +33,23 @@ def dynamic_url(name=None):
 #         message = data["message"]
 #         file = database.write(f'\n{name},{email},{subject},{message}')
 
-# def writetocsv(data):
-#     with open('database.csv',newline = '', mode='a') as database2:
-#         name = data["name"]
-#         email =  data["email"]
-#         subject = data["subject"]
-#         message = data["message"]
-#         csvwriter = csv.writer(database2, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-#         csvwriter.writerow([name,email,subject,message])
+def writetocsv(data):
+    with open('database.csv',newline = '', mode='a') as database2:
+        name = data["name"]
+        email =  data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csvwriter = csv.writer(database2, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csvwriter.writerow([name,email,subject,message])
 
-# @app.route('/submit_form', methods=['POST','GET'])
-# def submit_form():
-#     if request.method =='POST':
-#         try:
-#             data = request.form.to_dict()
-#             writetocsv(data)
-#             return 'form submitted'
-#         except:
-#             return 'did not save to database'
-#     else:
-#         return 'something went wrong'
+@app.route('/submit_form', methods=['POST','GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            writetocsv(data)
+            return redirect('submit_form.html')
+        except:
+            return 'did not save to database'
+    else:
+        return 'something went wrong'
